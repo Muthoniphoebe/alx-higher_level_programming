cc78c6e579a4ce9df711fe5aa5babc35e5425134
Muthoniphoebe <99501695+Muthoniphoebe@users.noreply.github.com>
2022-09-22 21:54:07 +0300

    updated code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 if __name__ == "__main__":
     import sys
 
-    tal = len(sys.argv)
-    if tal == 1:
-        print("{} arguments.".format(tal - 1))
-    elif tal == 2:
-        print("{} argument:".format(tal - 1))
+    tal = len(sys.argv) - 1
+    if tal == 0:
+        print("0 arguments.")
+    elif tal == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(tal - 1))
+        print("{} arguments:".format(tal))
 
-    for i in range(1, tal):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(tal):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
