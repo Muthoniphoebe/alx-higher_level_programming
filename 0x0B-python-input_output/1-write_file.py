beb0b7a9371505dc43a24bc782088d748626b02e
Muthoniphoebe <99501695+Muthoniphoebe@users.noreply.github.com>
2022-10-26 04:16:29 +0300

    updated code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
-"""Defines a text file line-counting function."""
+"""Module 2-read_lines.
+Reads a certain number of lines from a file.
+"""
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
+
+def read_lines(filename="", nb_lines=0):
+    """Reads and prints nb_lines lines from filename.
+    Args:
+        - filename: name of the file
+        - nb_lines: number of lines to read
+    """
+
     with open(filename) as f:
+        i = 0
+        count = 0
         for line in f:
-            lines += 1
-    return lines
+            count += 1
+        f.seek(0)
+        if nb_lines <= 0 or nb_lines >= count:
+            read_text = f.read()
+            print(read_text, end="")
+        else:
+            for line in f:
+                print(line, end='')
+                i += 1
+                if i == nb_lines:
+                    break
