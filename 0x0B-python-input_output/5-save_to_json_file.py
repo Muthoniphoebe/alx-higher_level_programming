0c143c927526fdce003e755d0c1e72ea9459fe4d
Muthoniphoebe <99501695+Muthoniphoebe@users.noreply.github.com>
2022-10-26 04:32:27 +0300

    updated code

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-Json Module
-"""
+"""Defines a JSON file-writing function."""
 import json
 
 
-def from_json_string(my_str):
-    """convert str to object
-    args:
-        my_str: string to convert
-    return:
-        object
-    """
-
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
